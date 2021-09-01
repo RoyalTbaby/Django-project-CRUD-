762d7dda1d8e8372e3022abb3861fc29fb7c500f
MARIABODOWSKAYA\Мария Бодовская <mashabodovskaya@gmail.com>
2021-09-02 00:12:30 +0200

    Created my app and added the models( id,firstname, lastname, age, profession). Adjusted the admin panel. Superuser

---
+++ mysite/store/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class User(models.Model):
+    jobs = (
+        ('Backend Developer', 'BACKEND DEVELOPER'),
+        ('IT-Admin', 'IT-ADMIN'),
+        ('Sales Representative', 'SALES REPRESENTATIVE'),
+        ('Support', 'SUPPORT'),
+        ('DevOps Engineer', 'DEVOPS ENGINEER'),
+        ('Frontend developer', 'FRONTEND DEVELOPER'),
+        ('Human Resources', 'HUMAN RESOURCES')
+    )
+    id = models.CharField(max_length=100, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    profession = models.CharField(max_length=100, choices=jobs)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
